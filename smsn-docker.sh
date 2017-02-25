880e122dd2ecf578267e9320978c0e84f44139a8
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-02-25 14:34:43 -0800

    first commit: three smsn config files and a script

---
+++ smsn-docker.sh
@@ -0,0 +1,40 @@
+## (one way) to run this file
+  ## sudo docker run -it --name smsn-docker -v /home/jeff/apps/docker/smsn-docker/context:/mnt ubuntu bash
+  ## (and then from within the Docker shell)
+  ## bash /mnt/smsn-docker.sh
+
+TINKERPOP_VERSION=3.2.4
+GREMLIN=apache-tinkerpop-gremlin-server-$TINKERPOP_VERSION
+
+apt update
+apt install -y git wget unzip
+apt install -y default-jre default-jdk # java
+
+
+               ## Semantic Synchrony
+apt install -y maven
+cd /root
+git clone https://github.com/joshsh/smsn.git
+cd /root/smsn
+mvn clean install
+
+
+               ## Tinkerpop
+cd /root
+
+if [ -f /mnt/$GREMLIN-bin.zip ]; then
+    cp /mnt/$GREMLIN-bin.zip .
+else
+    wget "http://www-eu.apache.org/dist/tinkerpop/3.2.4/$GREMLIN-bin.zip"
+fi
+
+unzip $GREMLIN-bin.zip 
+rm $GREMLIN-bin.zip
+
+cd $GREMLIN
+bin/gremlin-server.sh -i org.apache.tinkerpop neo4j-gremlin 3.2.4
+
+mkdir -p ext/smsn/plugin
+cp /root/smsn/smsn-server/target/smsn-server-*-standalone.jar ext/smsn/plugin
+
+cp /mnt/gremlin-server-smsn.yaml /mnt/neo4j.properties /mnt/smsn.properties conf
