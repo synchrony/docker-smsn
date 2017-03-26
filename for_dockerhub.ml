ddb34ea040500af668d57c10fd7b409498f1dd31
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2017-03-25 20:44:19 -0700

    update documentation for simpler start process

--- for_dockerhub.ml
+++ for_dockerhub.ml
@@ -8,12 +8,11 @@ Docker is supposedly easy to use in Windows and Mac, too. These steps work in Li
 * Create a folder on your machine where the SmSn Neo4j graph and activity log will reside. Those contents don't have to exist yet.
 * From a shell, run
 ```
-docker run -v /THE/FOLDER/YOU/MADE:/mnt/smsn-data -p 8182:8182 -d -h 127.0.0.1 jeffreybbrown/smsn:2017-03-19.1
+docker run -v /THE/FOLDER/YOU/MADE:/mnt/smsn-data -p 8182:8182 -d -h 127.0.0.1 jeffreybbrown/smsn
 ```
-(Substitute the path to the folder you made (including its name) where you see /THE/FOLDER/YOU/MADE.) This will bring up a shell within the Docker container. It's like another computer inside your computer.
-* In the Docker container, navigate to `/root/gremlin` and run `start.sh`.
-Now Gremlin Server is running, using Semantic Synchrony to serve the graph you gave it access to in the first step, at http://localhost:8182. When you terminate the Docker container, its data will vanish, but the graph at PATH will still be there.
-* All that remains is to install Emacs and configure it to work with Semantic Synchrony -- see steps 6 and 7 [here](https://github.com/joshsh/smsn/wiki/Extend-o-Brain-installation).
+(Substitute the path to the folder you made (including its name) where you see /THE/FOLDER/YOU/MADE.)
+* Now Gremlin Server is running (inside a Docker virtual machine), using Semantic Synchrony to serve the graph you gave it access to in the first step, at http://localhost:8182. When you terminate the Docker container, its data will vanish, but the graph at PATH will still be there.
+* All that remains is to install Emacs and configure it to work with Semantic Synchrony -- see steps 6 and 7 [here](https://github.com/joshsh/smsn/wiki/installation).
 
 ## How it was built + history of changes
 * https://github.com/JeffreyBenjaminBrown/docker-smsn
